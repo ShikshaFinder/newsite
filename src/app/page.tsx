ca70bc943ce54eef45a44af70bc77472d00f3bc2
ShikshaFinder <janiharsh794@gmail.com>
2024-08-01 13:28:59 +0530

    hi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { FlipWords } from "../components/ui/flip-words";
 import { Compare } from "../components/compare";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+import { WobbleCard } from "@/components/ui/wobble-card";
+import Image from "next/image";
+
+
 
 export default function FlipWordsDemo() {
   const words = ["Beautiful", "AI Powered", "Performant", "modern"];
@@ -8,13 +14,14 @@ export default function FlipWordsDemo() {
   return (
     <>
       {" "}
-      <div className="h-[40rem] flex justify-center items-center px-4">
+      {/* <div style={{display:"flex",alignContent:"center"}}> */}
+      <div className="h-[40rem] flex justify-center items-center px-4 ">
         <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
           Build
           <FlipWords words={words} /> <br />
           websites with GWSF Ventures PVT LTD.
         </div>
-        <div className="p-4 border rounded-3xl dark:bg-neutral-900 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
+        <div className="p-4 border rounded-3xl dark:bg-neutral-600 bg-neutral-100  border-neutral-200 dark:border-neutral-800 px-4">
           <Compare
             firstImage="https://assets.aceternity.com/code-problem.png"
             secondImage="https://assets.aceternity.com/code-solution.png"
@@ -25,6 +32,59 @@ export default function FlipWordsDemo() {
           />
         </div>
       </div>
+      <ShootingStars />
+      <StarsBackground />
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
+        <WobbleCard
+          containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
+          className=""
+        >
+          <div className="max-w-xs">
+            <h2 className="text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              Gippity AI powers the entire universe
+            </h2>
+            <p className="mt-4 text-left  text-base/6 text-neutral-200">
+              With over 100,000 mothly active bot users, Gippity AI is the most
+              popular AI platform for developers.
+            </p>
+          </div>
+          <Image
+            src="/linear.webp"
+            width={500}
+            height={500}
+            alt="linear demo image"
+            className="absolute -right-4 lg:-right-[40%] grayscale filter -bottom-10 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+        <WobbleCard containerClassName="col-span-1 min-h-[300px]">
+          <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+            No shirt, no shoes, no weapons.
+          </h2>
+          <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+            If someone yells “stop!”, goes limp, or taps out, the fight is over.
+          </p>
+        </WobbleCard>
+        <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-#FFF-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+          <div className="max-w-sm">
+            <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
+              Signup for blazing-fast cutting-edge state of the art Gippity AI
+              wrapper today!
+            </h2>
+            <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
+              With over 100,000 mothly active bot users, Gippity AI is the most
+              popular AI platform for developers.
+            </p>
+          </div>
+          <Image
+            src="/linear.webp"
+            width={500}
+            height={500}
+            alt="linear demo image"
+            className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+          />
+        </WobbleCard>
+      </div>
+      {/* </div> */}
     </>
   );
 }
